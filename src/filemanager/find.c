7d22bba010157d66c42f545cce534ba3b6f78713
Jan Kalisek <kalisek.jan@gmail.com>
2014-03-15 10:50:00 +0100

    Zmeny find.c finalni

--- src/filemanager/find.c
+++ src/filemanager/find.c
@@ -516,16 +516,16 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
     const char *file_name_label = N_("File name:");
     const char *file_recurs_label = N_("&Find recursively");
 
-    
+    const char *file_pattern_label = N_("&Using shell patterns");
 
-   /* const char *file_skip_hidden_label = N_("S&kip hidden");*/
+    const char *file_skip_hidden_label = N_("S&kip hidden");
     const char *file_only_directories_label = N_("Only &directories");
 
 #ifdef HAVE_CHARSET
     const char *file_all_charsets_label = N_("&All charsets");
 #endif
     const char *file_case_label = N_("Cas&e sensitive");
-    const char *file_skip_hidden_label = N_("S&kip hidden");
+   
 
     /* file content */
     const char *content_content_label = N_("Content:");
@@ -564,7 +564,7 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
         file_all_charsets_label = _(file_all_charsets_label);
 #endif
         file_case_label = _(file_case_label);
-        file_skip_hidden_label = _(file_skip_hidden_label);
+
 
         /* file content */
         content_content_label = _(content_content_label);
@@ -672,11 +672,9 @@ find_parameters (char **start_dir, ssize_t * start_dir_len,
     content_use_cbox = check_new (y2++, x2, options.content_use, content_use_label);
     add_widget (find_dlg, content_use_cbox);
 
-    cbox_position = FIND_Y - 5;
 
-    only_directories_cbox = check_new (cbox_position--, 3, options.only_directories, file_only_directories_label);
+    only_directories_cbox = check_new (y1++, x1, options.only_directories, file_only_directories_label);
     add_widget (find_dlg, only_directories_cbox);
-
 
     /* Continue 1st column */
     recursively_cbox = check_new (y1++, x1, options.find_recurs, file_recurs_label);
